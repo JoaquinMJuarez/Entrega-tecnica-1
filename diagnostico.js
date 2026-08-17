f92a4c2b6f85a5c7b3c6f547c4af8c25dfc3d5aa
Joaquin Juarez <joaquinmjuarez26@gamil.com>
2026-08-16 22:58:53 -0300

    feat: crear diagnostico.js

---
+++ diagnostico.js
@@ -0,0 +1,7 @@
+console.log("Versión de Node.js:", process.version);
+
+console.log("Plataforma:", process.platform);
+
+console.log("Argumentos:", process.argv);
+
+console.log("Variable de entorno USERNAME:", process.env.USERNAME);
